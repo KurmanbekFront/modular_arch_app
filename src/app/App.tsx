837e4f5b6201d2e9277037cd605d2a2678778e21
KurmanbekFront <nurkuluulukurmanbek53@gmail.com>
2025-06-10 15:57:46 +0600

    add components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,10 @@ import { CustomButton } from 'ui/button'
 function App() {
   return (
     <>
-      <div>
-        <CustomButton children="Button" />
-      </div>
+      <CustomButton children="Кнопка 1" type="button" />
+      <CustomButton children="Кнопка 2" type="submit" size="sm" variant="secondary" />
+      <CustomButton children="Кнопка 3" type="reset" size="md" variant="danger" />
+      <CustomButton children="Кнопка 4" type="button" size="lg" disabled={true} />
     </>
   )
 }
