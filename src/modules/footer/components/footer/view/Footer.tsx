69f47a5b4cda52dd89941de4f7bba2307c3b97d1
KurmanbekFront <nurkuluulukurmanbek53@gmail.com>
2025-06-30 22:09:28 +0600

    fix links

--- src/modules/footer/components/footer/view/Footer.tsx
+++ src/modules/footer/components/footer/view/Footer.tsx
@@ -9,33 +9,37 @@ export const Footer: FC = () => {
       <div>
         <Link to="/">LOGO</Link>
         <a
-          href="https://2gis.kg/bishkek/search/%D0%91%D0%B5%D0%BB%D1%8B%D0%B9%20%D0%B4%D0%BE%D0%BC?m=74.829832%2C42.828275%2F9.59" // ← сюда твоя ссылка на 2GIS
+          href="https://2gis.kg/bishkek/search/%D0%91%D0%B5%D0%BB%D1%8B%D0%B9%20%D0%B4%D0%BE%D0%BC?m=74.829832%2C42.828275%2F9.59"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Белый дом
+          Белый дом карта
         </a>
+        <div>
+          <a href="mailto:/">Email</a>
+          <a href="telegram">Telegram</a>
+          <a href="instagram">Instagram</a>
+          <a href="youtube">YouTube</a>
+        </div>
 
-        <a href="mailto:/">Email Link</a>
-        <a href="telegram">Telegram</a>
-        <a href="instagram">Instagram</a>
-        <a href="youtube">YouTube</a>
-
-        <a href="*">Министерство экономики</a>
-        <a href="*">Торгово-промышленная палата</a>
-
-        <a href="*">Часто задаваемые вопросы</a>
+        <div>
+          <a href="*">Министерство экономики</a>
+          <a href="*">Торгово-промышленная палата</a>
+          <a href="*">Часто задаваемые вопросы</a>
+        </div>
       </div>
 
       <Navbar>
-        <a href="tel:*">Телефон</a>
-        <a href="mailto:/">Электронная почта</a>
-        <span>Адрес</span>
+        <div className={styles.contactdata}>
+          <a href="tel:*">Телефон</a>
+          <a href="mailto:/">Электронная почта</a>
+          <span>Адрес</span>
+        </div>
       </Navbar>
       <small>&copy;2025 Все права защищены.</small>
       <button>Связаться</button>
-      <a>Политика конфиденциальности</a>
-      <a>Пользовательское соглашение</a>
+      <a href="privacy-policy*">Политика конфиденциальности</a>
+      <a href="useragreement">Пользовательское соглашение</a>
     </footer>
   )
 }
